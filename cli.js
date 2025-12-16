25e7cd9b64e0cf7f304f43e9f9e81e5d14aaedcb
Spirule-dev <matthieu.fergola@gmail.com>
2025-12-16 19:57:58 +0100

    fix maestro

--- cli.js
+++ cli.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
 import { createApp } from './src/createApp.js'
-await createApp()
+
+(async () => {
+  await createApp()
+})().catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
