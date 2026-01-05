88f6ad457fa156040316e00dfd16542f25d63215
Spirule-dev <matthieu.fergola@gmail.com>
2026-01-05 10:46:21 +0100

    update migration system to new version with new features

--- template/migrations/migration_0000000000000.sql
+++
@@ -1,4 +0,0 @@
--- Migration exemple :
--- USE `you_db_name`;
--- DROP TABLE IF EXISTS `user`;
--- CREATE TABLE IF NOT EXISTS `user` (`id` BIGINT AUTO_INCREMENT NOT NULL, `username` VARCHAR (255)  NOT NULL , `firstname` VARCHAR (255)  NOT NULL , `lastname` VARCHAR (255)  NOT NULL , `email` VARCHAR (255)  NOT NULL UNIQUE, `password` VARCHAR (255)  NOT NULL , `role` VARCHAR (255)  NOT NULL , `refresh_token` VARCHAR (255)   , `created_at` TIMESTAMP  NOT NULL , `updated_at` TIMESTAMP  NOT NULL , PRIMARY KEY (`id`));
